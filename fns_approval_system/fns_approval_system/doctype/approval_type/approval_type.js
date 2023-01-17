9ea1170efbf86b6e675623c5f0d3ae3b7eac8407
John Park <yuntan0@gmail.com>
2023-01-17 04:44:25 +0000

    Approval system update

---
+++ fns_approval_system/fns_approval_system/doctype/approval_type/approval_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, John Park and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Approval Type', {
+	// refresh: function(frm) {
+
+	// }
+});
